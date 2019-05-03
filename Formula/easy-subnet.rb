417418d6b6d3f66df8f647ea16e2a175fc19082b
Sathyavijayan Vittal <sathyavijayan@gmail.com>
2019-05-03 16:39:11 +0100

    fix

--- Formula/easy-subnet.rb
+++ Formula/easy-subnet.rb
@@ -1,7 +1,7 @@
 class EasySubnet < Formula
   desc "A command-line tool for easy split subnets into equally sized networks"
   homepage "https://github.com/BrunoBonacci/easy-subnet.git"
-  url "https://github.com/BrunoBonacci/easy-subnet/releases/download/0.4.1/easy-subnet-Darwin-x86_64"
+  url "https://github.com/sathyavijayan/homebrew-kapi/releases/download/0.4.1/easy-subnet-0.4.1.tar.gz"
   sha256 "45dba5ad841725c204ca5859bfc9c9b89f447249be2c7a303fb7da08a0de40d1"
   version "0.4.1"
 
